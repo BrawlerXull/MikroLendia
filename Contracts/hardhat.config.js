761177dfc9d13fe6bf7a4ae4edd33112e77aae0f
Dhairyash Jain <jaindhairyashj@gmail.com>
2025-01-19 02:09:00 +0530

    add-avalanche

--- Contracts/hardhat.config.js
+++ Contracts/hardhat.config.js
@@ -6,13 +6,16 @@ module.exports = {
   settings: {
     optimizer: {
       enabled: true,
-      runs: 200
+      runs: 200,
     },
-    viaIR: true
+    viaIR: true,
   },
-  networks:{
-    holesky: {
-    url: "https://1rpc.io/holesky",
-    accounts: [`0xd2fe688cd49e4d3fb2b2c79087e61da266413ce1b0aca83c56966d7eb0f3ef84`],
-  }}
+  networks: {
+    fuji: {
+      url: "https://ava-testnet.public.blastapi.io/ext/bc/C/rpc",
+      accounts: [
+        `0x4f9735f7c9dee0276d481e301e6ea8b027f2a6c5cd03f6aca64bedc3e36229ea`,
+      ],
+    },
+  },
 };
